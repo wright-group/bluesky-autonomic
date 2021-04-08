fb620580abcc68761074520607fd8cb1b6b1735c
Blaise Thompson <blaise@untzag.com>
2021-04-08 14:21:54 -0500

    opa skeleton

--- bluesky_autonomic/_opa_device.py
+++ bluesky_autonomic/_opa_device.py
@@ -12,9 +12,25 @@ class OPADevice(object):
         self._wrapped_device = device
         sdc_manager.register_opa(self)
 
+    @property
+    def arrangement(self) -> str:
+        raise NotImplementedError
+
     def describe(self) -> Dict["str", dict]:
         raise NotImplementedError
 
     @property
-    def arrangement(self) -> str:
-        raise NotImplementedError
+    def name(self):
+        return self._wrapped_device.name
+
+    @property
+    def position(self) -> float:
+        return self._wrapped_device.position
+
+    def read(self) -> Dict["str", dict]:
+        return self._wrapped_device.read
+
+    def set(self, position: float) -> Status:
+        sdc_manager.on_opa_set(self.name, position)
+        # TODO: join delay status to this status
+        return self._wrapped_device.set(position)
