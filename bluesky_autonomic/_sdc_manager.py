40cbb1eefc094be2c0432ef5d0bb5c3f132c607a
Kyle Sunden <git@ksunden.space>
2021-04-08 13:13:58 -0500

    method stubs sdc_manager

--- bluesky_autonomic/_sdc_manager.py
+++ bluesky_autonomic/_sdc_manager.py
@@ -1,7 +1,18 @@
+__all__ = ["sdc_manager"]
 
+import attune
 
 class SDCManager:
-    pass
+    def __init__(self):
+        pass
 
+    def register_device(self, device):
+        ...
 
-manager = SDCManager()
+    def set_correcation_enabled(self, opa, delay, enable):
+        ...
+
+    def on_opa_set(self, opa, destination):
+        ...
+
+sdc_manager = SDCManager()
