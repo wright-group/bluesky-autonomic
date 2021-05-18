740657e1bf484c2b1a3389f13263a4bf2299d29b
Kyle Sunden <git@ksunden.space>
2021-05-18 03:28:38 -0500

    Add subdevices for individual motors, set_postion_except

--- bluesky_autonomic/_opa_device.py
+++ bluesky_autonomic/_opa_device.py
@@ -7,12 +7,15 @@ from ._sdc_manager import sdc_manager
 from ._status import Status
 
 
-class OPADevice(object):
+class OPADevice:
 
     def __init__(self, device):
         self._wrapped_device = device
         sdc_manager.register_opa(self)
         self.parent = None
+        yaqclient = self._wrapped_device.yaq_client
+        for mot in yaqclient.get_setable_names():
+            setattr(self, mot, OPAMotor(self, mot))
 
     @property
     def arrangement(self) -> str:
@@ -43,3 +46,46 @@ class OPADevice(object):
         sdc_manager.on_opa_set(self.name)
         # TODO: join delay status to this status
         return ret
+
+    def set_position_except(self, position: float, exceptions=None):
+        if exceptions is None:
+            exceptions = []
+        ret = self._wrapped_device.set_position_except(position, exceptions)
+        sdc_manager.on_opa_set(self.name)
+        # TODO: join delay status to this status
+
+class OPAMotor:
+
+    def __init__(self, device: OPADevice, motor: str):
+        self._wrapped_device = device._wrapped_device
+        self._motor = motor
+        self.parent = device
+
+    def describe(self) -> Dict["str", dict]:
+        return {
+                self.name: {"source":f"bluesky-autonomic:{self.name}",
+                                    "shape":(),
+                                    "dtype": "number",
+                                    }
+                }
+
+
+    def describe_configuration(self) -> Dict["str", dict]:
+        return {}
+
+    def read_configuration(self) -> Dict["str", dict]:
+        return {}
+
+    @property
+    def name(self):
+        return f"{self._wrapped_device.name}.{self._motor}"
+
+    @property
+    def position(self) -> float:
+        return self._wrapped_device.yaq_client.get_setable_positions()[self._motor]
+
+    def read(self) -> Dict["str", dict]:
+        return {self.name: self.position}
+
+    def set(self, position: float) -> Status:
+        return self._wrapped_device.yaq_client.set_setable_positions({self._motor:position})
