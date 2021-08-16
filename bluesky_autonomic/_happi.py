3f9de7189720af62f4e41c68157bf35e5a5bc783
Blaise Thompson <blaise@untzag.com>
2021-08-16 16:12:35 -0500

    add

---
+++ bluesky_autonomic/_happi.py
@@ -0,0 +1,25 @@
+"""Support for happi -
+pcdshub.github.io/happi
+"""
+
+
+import re
+import copy
+
+from happi.item import HappiItem, EntryInfo  # type: ignore
+
+
+class DelayItem(HappiItem):
+    port = EntryInfo("TCP port.", enforce=int, optional=False)
+    host = EntryInfo("Host.", optional=True, default="localhost")
+    kwargs = copy.copy(HappiItem.kwargs)
+    kwargs.default = {"port": "{{port}}", "host": "{{host}}", "name": "{{name}}"}
+    device_class = EntryInfo(default="yaqc_bluesky.Device")
+
+
+class OPAItem(HappiItem):
+    port = EntryInfo("TCP port.", enforce=int, optional=False)
+    host = EntryInfo("Host.", optional=True, default="localhost")
+    kwargs = copy.copy(HappiItem.kwargs)
+    kwargs.default = {"port": "{{port}}", "host": "{{host}}", "name": "{{name}}"}
+    device_class = EntryInfo(default="yaqc_bluesky.Device")
